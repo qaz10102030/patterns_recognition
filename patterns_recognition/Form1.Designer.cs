3d0f4d9ae903c2224e7fda4c94a053d27d6769ce
Rabbit-laptop <qaz1010203044@gmail.com>
2017-10-23 18:04:06 +0800

    first push

---
+++ patterns_recognition/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace patterns_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.Interval = 1D;
+            chartArea3.AxisX.MajorGrid.Enabled = false;
+            chartArea3.AxisY.Interval = 10D;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(13, 100);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(909, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "n=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "d=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "prob1=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(319, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "prob2=";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(319, 43);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 412);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ patterns_recognition/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace patterns_recognition
+{
+    public partial class Form1 : Form
+    {
+        public Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        public int[] count = new int[] { 0, 0, 0, 0 };
+        private readonly Color[] _colors = new Color[] { Color.Peru, Color.PowderBlue };
+        public int[] result = new int[1001];
+        public int[] result2 = new int[1001];
+        public double[] bernoArray = new double[1001];
+        public double[] bernoArray2 = new double[1001];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = 0;
+                result2[i] = 0;
+                bernoArray[i] = 0;
+                bernoArray2[i] = 0;
+            }
+            double[] prob = new double[] { 0.25, 0.5 };
+            int n = Int16.Parse(textBox1.Text), d = Int16.Parse(textBox2.Text) ;
+            test(d, n, prob);
+            draw(result, result2, prob, d, n,bernoArray,bernoArray2);
+        }
+
+        public void test(int d,int n,double[] prob)
+        {
+            count[0] = 0;
+            count[1] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= d; j++)
+                {
+                    double num = rnd.NextDouble();
+                    if (num <= prob[0]) count[0]++;
+                }
+                result[count[0]]++;
+                count[0] = 0;
+            }
+            for (int j = 1; j <= d; j++)
+            {
+                bernoArray[j] = bernoulli(d, result[j], prob[0]);
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= d; j++)
+                {
+                    double num = rnd.NextDouble();
+                    if (num <= prob[1]) count[1]++;
+                }
+                result2[count[1]]++;
+                count[1] = 0;
+            }
+            for (int j = 1; j <= d; j++)
+            {
+                bernoArray2[j] = bernoulli(d, result2[j], prob[1]);
+            }
+        }
+
+        public void draw(int[] _y, int[] _y2, double[] prob, int _length, int time, double[] _bernoArray, double[] _bernoArray2)
+        {
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+            chart1.Titles.Add("d=" + _length + ",n=" + time);
+            Series _series = new Series();
+            for (int index = 1; index <= _length; index++)
+            {
+                _series.Color = Color.DodgerBlue;
+                _series.ChartType = SeriesChartType.Column;
+                _series.IsValueShownAsLabel = true;
+                 _series.Name ="prob=" + prob[0];
+                if (_y[index] != 0)
+                {
+                    _series.Points.AddXY(index, _y[index]);
+                }
+            }
+            chart1.Series.Add(_series);
+
+            _series = new Series();
+            for (int index = 1; index <= _length; index++)
+            {
+                _series.ChartType = SeriesChartType.Column;
+                _series.Color = Color.Red;
+                _series.IsValueShownAsLabel = true;
+                _series.Name ="prob=" + prob[1];
+                if (_y2[index] != 0)
+                {
+                    _series.Points.AddXY(index, _y2[index]);
+                }
+            }
+            chart1.Series.Add(_series);
+        }
+
+        public double bernoulli(int d, int time, double prob)
+        {
+            double bernoRes = 0.0;
+            bernoRes = Math.Pow(prob, time) * Math.Pow(1 - prob, d - time);
+                return bernoRes;
+        }
+
+    }
+}
